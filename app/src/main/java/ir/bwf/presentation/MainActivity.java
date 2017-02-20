0caf88b049d0be43269f8591b17b8b24efde6b37
success0 <elahe.ofu@GMAIL.COM>
2017-02-20 16:50:38 +0330

    my commit message

---
+++ app/src/main/java/ir/bwf/presentation/MainActivity.java
@@ -0,0 +1,45 @@
+package ir.bwf.presentation;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Menu;
+
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the view from activity_main.xml
+        setContentView(R.layout.activity_main);
+
+        // Locate the button in activity_main.xml
+        button = (Button) findViewById(R.id.btn_parash);
+
+        // Capture button clicks
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        Parash.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/ir/bwf/presentation/Parash.java
@@ -0,0 +1,19 @@
+package ir.bwf.presentation;
+
+/**
+ * Created by Administrator on 2/20/2017.
+ */
+import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+
+
+public class Parash extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.parash);
+    }
+}
